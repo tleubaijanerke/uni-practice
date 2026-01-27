0c539bc146f855839a5e9577e87ebbd3b9c32c2b
zhanerke tileubay <tileubaijanerke@gmail.com>
2026-01-28 00:49:32 +0500

    Add examples for Numbers topic

---
+++ numbers.py
@@ -0,0 +1,34 @@
+#1
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+print(type(x))
+print(type(y))
+print(type(z))
+
+#2
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#3
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#4
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
