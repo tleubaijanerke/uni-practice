d3de05f690f0977bffd11d4ac27f7ba2aff4c040
zhanerke tileubay <tileubaijanerke@gmail.com>
2026-01-28 00:26:54 +0500

    Add 5 examples for Getting Started topic

---
+++ getstart.py
@@ -0,0 +1,22 @@
+#1
+print("Hello, World!")
+
+#2
+import sys
+print("Python version:", sys.version)
+
+#3
+a = 7
+b = 3
+print("Sum:", a + b)
+print("Difference:", a - b)
+print("Product:", a * b)
+print("Division:", a / b)
+
+#4
+name = input("Enter your name: ")
+print("Hello,", name)
+
+#5
+print(">>> print('Hello, World!')")
+print("Hello, World!")
