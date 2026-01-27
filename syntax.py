d16099ceaf48b7cd1ed46010ffd31118efbbe1ed
zhanerke tileubay <tileubaijanerke@gmail.com>
2026-01-28 00:33:03 +0500

    Add 5 examples for Execute Python Syntax topic

---
+++ syntax.py
@@ -0,0 +1,18 @@
+#1
+if 5 > 2:
+    print("Five is greater than two!")
+
+#2
+x = 5
+y = "Hello, World!"
+print(x)
+print(y)
+
+#3
+# This is a comment
+print("Comments do not affect code execution")
+
+#4
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
