b6bee0f18fbe883a13825e514942cd9e44bbdeff
zhanerke tileubay <tileubaijanerke@gmail.com>
2026-02-03 11:27:51 +0500

    Add Practice1 folder

---
+++ Practice1/comments.py
@@ -0,0 +1,20 @@
+#1
+# This is a comment, Python will ignore it
+print("Hello, World!")
+
+#2
+print("Hello, World!")  # This part is ignored by Python
+
+#3
+# print("This code will not run")
+print("Cheers, Mate!")
+
+#4
+# First comment
+# Second comment
+print("Multiple comments work fine")
+
+#5
+# Create a variable age and print it
+age = 17
+print("Age:", age)
