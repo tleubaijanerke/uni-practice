09ca1a61772c831458a7cd634e047fabe4d1dc2a
zhanerke tileubay <tileubaijanerke@gmail.com>
2026-02-26 01:29:00 +0500

    move files into practice4 folder

---
+++ Practice4/Practice4/math.py
@@ -0,0 +1,54 @@
+#1
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+print(x)
+print(y)
+
+#5
+#25
+
+
+#2
+x = abs(-7.25)
+print(x)
+
+#7.25
+
+
+#3
+import math
+x = math.sqrt(64)
+print(x)
+
+#8.0
+
+
+#4
+import math
+x = math.pi
+print(x)
+
+#3.141592653589793
+
+
+#5
+import random
+x = random.randint(1, 10)
+print(x)
+
+
+
+# min() / max() — find smallest / largest value
+# abs() — absolute value
+# round() — round a number to nearest integer
+# pow(a, b) — a raised to the power b
+
+# sqrt() — square root
+# ceil() / floor() — round up / down
+# sin(), cos() — trigonometric functions
+# pi, e — mathematical constants
+
+# random() — random float 0 ≤ x < 1
+# randint(a, b) — random integer between a and b
+# choice(seq) — random element from sequence
+# shuffle(seq) — shuffle a list in place
--- Practice4/json.py
+++
@@ -1,2 +0,0 @@
-x = random.randint(1, 10)
-print(x)
