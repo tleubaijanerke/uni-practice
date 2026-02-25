fb4c3bfa0b1b7364f9ff619637c9447e02dff30c
tleubaijanerke <tleubaijanerke@gmail.com>
2026-02-26 01:24:38 +0500

    Update dates.py

--- dates.py
+++ dates.py
@@ -1,43 +1,61 @@
 #1
 import datetime
-x = datetime.datetime.now()
-print(x)
 
-#2026-02-21 23:42:03.309990
+today = datetime.datetime.now()
+new_date = today - datetime.timedelta(days=5)
+
+print(new_date)
+
+#This program gets the current date and time using datetime.now().
+#It subtracts 5 days using datetime.timedelta(days=5).
+#Output: current date minus five days.
 
 
 #2
 import datetime
-x = datetime.datetime.now()
-print(x.year)
-print(x.strftime("%A"))
 
-#2026
-#Saturday
+today = datetime.date.today()
+
+yesterday = today - datetime.timedelta(days=1)
+tomorrow = today + datetime.timedelta(days=1)
+
+print("Yesterday:", yesterday)
+print("Today:", today)
+print("Tomorrow:", tomorrow)
+
+#This program uses date.today() to get the current date.
+#It calculates yesterday and tomorrow using timedelta.
+#Output: three dates — yesterday, today, tomorrow.
 
 
 #3
 import datetime
-x = datetime.datetime(2020, 5, 17)
-print(x)
 
-#2020-05-17 00:00:00
+now = datetime.datetime.now()
+without_microseconds = now.replace(microsecond=0)
+
+print(without_microseconds)
+
+#This program gets the current datetime.
+#It removes microseconds using .replace(microsecond=0).
+#Output: current datetime without microseconds.
 
 
 #4
 import datetime
-x = datetime.datetime(2018, 6, 1)
-print(x.strftime("%B"))
 
-#June
+date1 = datetime.datetime(2026, 2, 21, 12, 0, 0)
+date2 = datetime.datetime(2026, 2, 20, 12, 0, 0)
 
+difference = date1 - date2
 
-#5
-import datetime
-x = datetime.datetime.now()
-print(x.strftime("%Y"))
+print(difference.total_seconds())
 
-#2026
+#This program creates two datetime objects.
+#It subtracts them to get a timedelta object.
+#Then it converts the difference to seconds using .total_seconds().
+#Output: difference between two dates in seconds.
+
 
 
 # The datetime module is used to work with dates and time in Python.
