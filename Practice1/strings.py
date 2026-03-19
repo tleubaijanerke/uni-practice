0e31e2eb83f005b84e71891d78c875c9465d6e97
tleubaijanerke <tleubaijanerke@gmail.com>
2026-03-19 05:28:09 +0500

    Add files via upload

---
+++ Practice1/strings.py
@@ -0,0 +1,23 @@
+#1
+b = "Hello, World!"
+print(b[2:5])
+
+#2
+a = "Hello, World!"
+print(a.upper())
+
+#3
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+#4
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+#5
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
